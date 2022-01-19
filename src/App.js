ebfe2318394524d12eabc5c1afd3ce84337f32fa
khanhafe <khanhafe@gmail.com>
2022-01-19 12:46:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from "react";
+//import "./App.css";
+
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <Fragment>
+      <section className="hero is-light has-text-centered">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="is-size-1">
+              ReactJS multi step form with validation
+            </h1>
+          </div>
+        </div>
+      </section>
+      <div className="container pt-5">
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            <Form />
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
